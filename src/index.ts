0cea25ee5208369ec6ede15e8005bc1735c10d2a
Ramón Otero <kaptux@gmail.com>
2020-01-02 19:03:54 +0100

    Initial implementation

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export * from "./state-container"
+export * from "./use-action-effect"
+export * from "./use-object-state"
---
+++ src/use-action-effect.tsx
@@ -0,0 +1,22 @@
+import { useState, useEffect, useRef } from "react"
+
+export default function useActionEffect<T extends (...args: any[]) => void>(
+	action: T,
+	onInit = false,
+): T {
+	const [count, setCount] = useState(onInit ? 1 : 0)
+	const actionArgsRef = useRef([] as any[])
+
+	useEffect(() => {
+		if (count > 0) {
+			action(...actionArgsRef.current)
+		}
+	}, [count])
+
+	const res = (...args: any[]) => {
+		actionArgsRef.current = args
+		setCount(prevCount => prevCount + 1)
+	}
+
+	return res as T
+}
